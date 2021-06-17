2b632c1736e978557aca4445577455817567669e
Samantha <typesammy@gmail.com>
2021-06-17 10:33:59 +1000

    added password email and username checks in user model

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -6,13 +6,21 @@ post '/signup' do
   username = params[:username]
   email = params[:email]
   pw = params[:password]
-  existing_user_check = username_check(username)
- 
-  if existing_user_check == nil
+  db_username = db_username_check(username)
+
+  # validators
+  valid_email_check = valid_email_check(email)
+  valid_password_check = valid_pw_check(pw)
+  valid_user_check = valid_username_check(username, db_username)
+
+  if valid_email_check && valid_password_check && valid_user_check
     user_sign_up(username, email, pw)
     redirect "/login"
-  elsif username == existing_user_check
+  elsif valid_email_check == false
+    erb :"user/new", locals: { error: "Invalid Email" }
+  elsif valid_password_check == false
+    erb :"user/new", locals: { error: "Password must be 8 characters long, include one uppercase letter and one number" }
+  elsif valid_user_check == false
     erb :"user/new", locals: { error: "Username taken" }
   end
-
 end
--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,16 @@
 require "bcrypt"
 
+def valid_pw_check(pw)
+  pw_length = pw.length
+  pw_incl_upcase = /[[:upper:]]/.match(pw) == nil ? 0 : 1 #found this online!!
+  pw_incl_num = pw.count("0-9")
+  if pw_length >= 8 && pw_incl_upcase == 1 && pw_incl_num >= 1
+    true
+  else
+    false
+  end
+end
+
 def user_sign_up(username, email, pw)
   password_digest = BCrypt::Password.create(pw)
   sql_query = "INSERT INTO users(username, email, password_digest) VALUES($1, $2, $3);"
@@ -19,7 +30,7 @@ def find_user(column_name, value)
   end
 end
 
-def username_check(username_input)
+def db_username_check(username_input)
   username_in_db = find_user("username", username_input)
   if username_in_db != nil
     result = username_in_db["username"]
@@ -27,4 +38,38 @@ def username_check(username_input)
     result = nil
   end
   result
+end
+
+def valid_username_check(username_input, db_username)
+  if username_input == db_username
+    false
+  else
+    true
+  end
+end
+
+def valid_email_check(email_input)
+  one_at_sign = email_input.count "@"
+  include_decimal = email_input.count "."
+  split_at_sign = email_input.split "@"
+  include_decial_after_at_sign = split_at_sign.last.count "."
+  decimal_index = split_at_sign.last.index"."
+
+  if one_at_sign != 1
+    false
+  elsif include_decimal == 0
+    false
+  elsif split_at_sign.first.length == 0
+    false
+  elsif include_decial_after_at_sign < 1
+    false
+  elsif decimal_index == 0
+    false
+  elsif split_at_sign[1].nil?
+    false
+  elsif split_at_sign[1][-1] == "."
+    false
+  else
+    true
+  end
 end
